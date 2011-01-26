a4f3f13461ddd9d83051f14a5a3814d4ed5f6257
Torben Rasmussen <rtorben@gmail.com>
2011-01-26 08:46:09 +0000

    leds work, uart works

--- firmware/fcu/fcu.c
+++ firmware/fcu/fcu.c
@@ -1,166 +1,220 @@
 #include "fcu.h"
 
-/* USART Initialization */
-/*
-ISR(USART<XX>_RXC_vect)
-{
-    stdout = &debugOut;
-}
-*/
+static FILE usb_out = FDEV_SETUP_STREAM (putchar_usb, NULL, _FDEV_SETUP_WRITE);
 
 /* SPI Initialization */
-static void spi_init (void)
+//static void spi_init (void)
+//{
+//    /* Init signal select pins with wired AND pull-up. */
+//    PORTC.DIRSET = PIN4_bm;
+//    PORTC.PIN4CTRL = PORT_OPC_WIREDANDPULL_gc;
+//
+//    /* Set SS output to high. (No slave addressed). */
+//    PORTC.OUTSET = PIN4_bm;
+//
+//    /* Initialize SPI master on port C. */
+//    SPI_MasterInit(     &spiMasterC,
+//                        &SPIC,
+//                        &PORTC,
+//                        false,
+//                        SPI_MODE_0_gc,
+//                        SPI_INTLVL_LO_gc,
+//                        false,
+//                        SPI_PRESCALER_DIV4_gc);
+//    /* Enable low and medium level interrupts in the interrupt controller. */
+//    PMIC.CTRL |= PMIC_MEDLVLEN_bm | PMIC_LOLVLEN_bm;
+//    sei();
+//
+//	/* Create data packet (SS to slave by PC4) */
+//	SPI_MasterCreateDataPacket(&dataPacket,
+//	                           sendData,
+//	                           receivedData,
+//	                           NUM_BYTES + 1,
+//	                           &PORTC, //THESE SHOULD BE SET TO SS PINS
+//	                           PIN4_bm);
+//
+//	/* Transmit and receive first data byte. */
+//	uint8_t status;
+//	do {
+//		status = SPI_MasterInterruptTransceivePacket(&spiMasterC, &dataPacket);
+//	} while (status != SPI_OK);
+//
+//	/* Wait for transmission to complete. */
+//	while (dataPacket.complete == false) {
+//
+//	}
+//
+//	/* Check that correct data was received. Assume success at first. */
+//	success = true;
+//    uint8_t i;
+//	for (i = 0; i < NUM_BYTES; i++) {
+//		if (receivedData[i + 1] != (uint8_t)(sendData[i] + 1)) {
+//			success = false;
+//		}
+//	}
+//	while(true) {
+//		nop();
+//	}
+//}
+
+//ISR(SPIC_INT_vect)
+//{
+//	SPI_MasterInterruptHandler(&spiMasterC);
+//}
+//
+//ISR(SPID_INT_vect)
+//{
+//	/* Get received data. */
+//	uint8_t data = SPI_SlaveReadByte(&spiSlaveD);
+//
+//	/* Increment data. */
+//	data++;
+//
+//	/* Send back incremented value. */
+//	SPI_SlaveWriteByte(&spiSlaveD, data);
+//}
+
+void init_xbee_uart (int8_t bScale, uint16_t bSel) 
 {
-    /* Init signal select pins with wired AND pull-up. */
-    PORTC.DIRSET = PIN4_bm;
-    PORTC.PIN4CTRL = PORT_OPC_WIREDANDPULL_gc;
+    PORTF.DIRCLR    =   PIN2_bm;
+    PORTF.DIRSET    =   PIN3_bm;
 
-    /* Set SS output to high. (No slave addressed). */
-    PORTC.OUTSET = PIN4_bm;
+    /* USARTE0, 8 Data bits, No Parity, 1 Stop bit. */
+    USART_Format_Set (&USARTF0, USART_CHSIZE_8BIT_gc,
+                     USART_PMODE_DISABLED_gc, 0);
+                     
+    /* Enable RXC interrupt. */
+    USART_RxdInterruptLevel_Set (&USARTF0, USART_RXCINTLVL_LO_gc);
 
-    /* Initialize SPI master on port C. */
-    SPI_MasterInit(     &spiMasterC,
-                        &SPIC,
-                        &PORTC,
-                        false,
-                        SPI_MODE_0_gc,
-                        SPI_INTLVL_LO_gc,
-                        false,
-                        SPI_PRESCALER_DIV4_gc);
-    /* Enable low and medium level interrupts in the interrupt controller. */
-    PMIC.CTRL |= PMIC_MEDLVLEN_bm | PMIC_LOLVLEN_bm;
-    sei();
-
-	/* Create data packet (SS to slave by PC4) */
-	SPI_MasterCreateDataPacket(&dataPacket,
-	                           sendData,
-	                           receivedData,
-	                           NUM_BYTES + 1,
-	                           &PORTC, //THESE SHOULD BE SET TO SS PINS
-	                           PIN4_bm);
-
-	/* Transmit and receive first data byte. */
-	uint8_t status;
-	do {
-		status = SPI_MasterInterruptTransceivePacket(&spiMasterC, &dataPacket);
-	} while (status != SPI_OK);
-
-	/* Wait for transmission to complete. */
-	while (dataPacket.complete == false) {
-
-	}
-
-	/* Check that correct data was received. Assume success at first. */
-	success = true;
-    uint8_t i;
-	for (i = 0; i < NUM_BYTES; i++) {
-		if (receivedData[i + 1] != (uint8_t)(sendData[i] + 1)) {
-			success = false;
-		}
-	}
-	while(true) {
-		nop();
-	}
-}
-
-ISR(SPIC_INT_vect)
-{
-	SPI_MasterInterruptHandler(&spiMasterC);
-}
-
-ISR(SPID_INT_vect)
-{
-	/* Get received data. */
-	uint8_t data = SPI_SlaveReadByte(&spiSlaveD);
-
-	/* Increment data. */
-	data++;
-
-	/* Send back incremented value. */
-	SPI_SlaveWriteByte(&spiSlaveD, data);
-}
-
-static void initUarts (void)
-{
-	PORTD.DIRCLR   = PIN2_bm;
-	PORTD.DIRCLR   = PIN6_bm;
-	PORTC.DIRCLR   = PIN2_bm;
-	PORTC.DIRCLR   = PIN6_bm;
-	
-	// Set pins to inverting
-	PORTD.PIN2CTRL= 0b01000000;
-	PORTD.PIN6CTRL= 0b01000000;
-	PORTC.PIN2CTRL= 0b01000000;
-	PORTC.PIN6CTRL= 0b01000000;
- 
-    // BSEL = 51 so at 32mhz clock, baud rate should be 38400
-
-	/* USARTD0, 8 Data bits, No Parity, 2 Stop bit. */
-	USART_Format_Set(&USARTD0, USART_CHSIZE_8BIT_gc,
-                     USART_PMODE_DISABLED_gc, 1);
-	/* USARTD1, 8 Data bits, No Parity, 2 Stop bit. */
-	USART_Format_Set(&USARTD1, USART_CHSIZE_8BIT_gc,
-                     USART_PMODE_DISABLED_gc, 1);
-	/* USARTC0, 8 Data bits, No Parity, 2 Stop bit. */
-	USART_Format_Set(&USARTC0, USART_CHSIZE_8BIT_gc,
-                     USART_PMODE_DISABLED_gc, 1);
-	/* USARTC1, 8 Data bits, No Parity, 2 Stop bit. */
-	USART_Format_Set(&USARTC1, USART_CHSIZE_8BIT_gc,
-                     USART_PMODE_DISABLED_gc, 1);
- 
-    //~ USARTC0.BAUDCTRLB = 0;			// BSCALE = 0 as well
-    USARTC0.BAUDCTRLA = 0b00110111;
-    USARTC1.BAUDCTRLA = 0b00110111;
-    USARTD0.BAUDCTRLA = 0b00110111;
-    USARTD1.BAUDCTRLA = 0b00110111;
+    USART_Baudrate_Set (&USARTF0, bSel, bScale);
     
-    USARTC0.BAUDCTRLB = 0b10110100;
-    USARTC1.BAUDCTRLB = 0b10110100;
-    USARTD0.BAUDCTRLB = 0b10110100;
-    USARTD1.BAUDCTRLB = 0b10110100;
- 
-    /* Enable TX. */
-	USART_Tx_Enable(&USARTD0);
-	USART_Tx_Enable(&USARTD1);
-	USART_Tx_Enable(&USARTC0);
-	USART_Tx_Enable(&USARTC1);
+    /* Enable both RX and TX. */
+    USART_Rx_Enable (&USARTF0);
+    USART_Tx_Enable (&USARTF0);
+
+    /* Enable PMIC interrupt level low. */
+    PMIC.CTRL |= PMIC_LOLVLEX_bm;
 }
 
-/*
-static int putcharimu (char c, FILE *stream, USART_t usart)
+void init_usb_uart (int8_t bScale, uint16_t bSel) 
 {
-    if (c == '\n')
-        putcharimu('\r', stream);
-    while ( !( usart.STATUS & USART_DREIF_bm) ); // Wait for the transmit buffer to be empty
-    usart.DATA = c; // Put our character into the transmit buffer
-    return 0;
+    PORTC.DIRCLR    =   PIN6_bm;
+    PORTC.DIRSET    =   PIN7_bm;
+
+    /* USARTE0, 8 Data bits, No Parity, 1 Stop bit. */
+    USART_Format_Set (&USARTC1, USART_CHSIZE_8BIT_gc,
+                     USART_PMODE_DISABLED_gc, 0);
+                     
+    /* Enable RXC interrupt. */
+    USART_RxdInterruptLevel_Set (&USARTC1, USART_RXCINTLVL_LO_gc);
+
+    USART_Baudrate_Set (&USARTC1, bSel, bScale);
+    
+    /* Enable both RX and TX. */
+    USART_Rx_Enable (&USARTC1);
+    USART_Tx_Enable (&USARTC1);
+
+    /* Enable PMIC interrupt level low. */
+    PMIC.CTRL |= PMIC_LOLVLEX_bm;
 }
-*/
+
+//xbee
+ISR(USARTF1_RXC_vect) 
+{
+    unsigned char c = USARTF1.DATA;
+}
+
+//usb rx complete
+ISR(USARTC1_RXC_vect) 
+{
+    unsigned char c = USARTC1.DATA;
+    //putchar_usb(c);
+    stdout = &usb_out;
+    printf("Received: %c\n\r", c);
+}
+
+void putchar_xbee (char c) 
+{
+    while ( !( USARTF0.STATUS & USART_DREIF_bm) ); // Wait for the transmit buffer to be empty
+    USARTF0.DATA = c; // Put our character into the transmit buffer
+}
+
+void putchar_usb (char c) 
+{
+    while ( !( USARTC1.STATUS & USART_DREIF_bm) ); // Wait for the transmit buffer to be empty
+    USARTC1.DATA = c; // Put our character into the transmit buffer
+}
+
+void configClock (void) 
+{
+    // enable 32mhz oscillator 
+    CLKSYS_Enable( OSC_RC32MEN_bm );
+    do {} while ( CLKSYS_IsReady( OSC_RC32MRDY_bm ) == 0 );
+
+    // configure pll source = 32mhz oscillator/4 * 16 = 4*32mhz = 128mhz output
+    CLKSYS_PLL_Config( OSC_PLLSRC_RC32M_gc, 16 );
+
+    // enable pll
+    CLKSYS_Enable( OSC_PLLEN_bm );
+    do {} while ( CLKSYS_IsReady( OSC_PLLEN_bm ) == 0 );
+
+    // enable prescale by 2 and 2 again to generate 2x and 4x clocks
+    CCP = CCP_IOREG_gc;
+    CLK.PSCTRL = CLK_PSBCDIV_2_2_gc;
+
+    // select main clock source as pll output
+    CLKSYS_Main_ClockSource_Select( CLK_SCLKSEL_PLL_gc );
+    
+    // output clock on port d pin 7
+    //~ PORTCFG.CLKEVOUT = PORTCFG_CLKOUT_PD7_gc; 
+}
 
 int main (void) 
 {
+    configClock();
+    //set led pins as outputs
     PORTA.DIRSET=0b11110000;
     PORTF.DIRSET=0b11110000;
+
+    init_xbee_uart(-4, 3317); //32MHz, 9600 baud
+    init_usb_uart(-4, 3317); //32MHz, 9600 baud
+    sei();
     while(1)
     {
         LED_1_RED_ON();
-        LED_1_GREEN_ON();
-        LED_2_RED_ON();
-        LED_2_GREEN_ON();
-        LED_3_RED_ON();
-        LED_3_GREEN_ON();
-        LED_4_RED_ON();
-        LED_4_GREEN_ON();
-        _delay_ms(500);
+        _delay_ms(100);
         LED_1_RED_OFF();
+        _delay_ms(100);
+        LED_1_GREEN_ON();
+        _delay_ms(100);
         LED_1_GREEN_OFF();
+        _delay_ms(100);
+        LED_2_RED_ON();
+        _delay_ms(100);
         LED_2_RED_OFF();
+        _delay_ms(100);
+        LED_2_GREEN_ON();
+        _delay_ms(100);
         LED_2_GREEN_OFF();
+        _delay_ms(100);
+        LED_3_RED_ON();
+        _delay_ms(100);
         LED_3_RED_OFF();
+        _delay_ms(100);
+        LED_3_GREEN_ON();
+        _delay_ms(100);
         LED_3_GREEN_OFF();
+        _delay_ms(100);
+        LED_4_RED_ON();
+        _delay_ms(100);
         LED_4_RED_OFF();
+        _delay_ms(100);
+        LED_4_GREEN_ON();
+        _delay_ms(100);
         LED_4_GREEN_OFF();
-        _delay_ms(500);
+        _delay_ms(100);
+        //putchar_xbee('x');
+        //putchar_usb('x');
     }
-    return;
+    return 0;
 }
--- firmware/fcu/fcu.h
+++ firmware/fcu/fcu.h
@@ -5,9 +5,12 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <math.h>
+#include <avr/interrupt.h>
 
 #include "spi_driver.h"
 #include "avr_compiler.h"
+#include "usart_driver.h"
+#include "clksys_driver.h"
 
 /* Led bitmasks */
 //#define LED_MISC_4_GREEN() PORTF.OUTCLR=PIN4_bm;PORTF.OUTSET=PIN5_bm
@@ -39,12 +42,10 @@ static void uart_cpu_init (void);
 static void uart_xbee_init (void);
 static void uart_sonar_init (void);
 
-/*
-static USART_t usbUSART = &USART<XX>
-static USART_t cpuUSART = &USART<XX>
-static USART_t xbeeUSART = &USART<XX>
-static USART_t sonarUSART = &USART<XX>
-*/
+void init_xbee_uart (int8_t bScale, uint16_t bSel);
+void init_usb_uart (int8_t bScale, uint16_t bSel);
+void putchar_xbee (char c);
+void putchar_usb (char c);
 
 volatile uint8_t mcu_tx_packet[8];
 volatile uint8_t mcu_rx_packet[8];
@@ -52,24 +53,25 @@ volatile uint8_t mcu_rx_packet[8];
 volatile uint8_t imu_tx_packet[1];
 volatile uint8_t imu_rx_packet[16];
 
-/*** APP NOTE CODE ***/
-/*! \brief Number of test data bytes. */
-#define NUM_BYTES   2
-/* Global variables. */
-/*! \brief SPI master on PORT C. */
-SPI_Master_t spiMasterC;
-/*! \brief SPI slave on PORT D. */
-SPI_Slave_t spiSlaveD = {NULL, NULL};
-/*! \brief Data packet. */
-SPI_DataPacket_t dataPacket;
-/*! \brief Test data to send. */
-const uint8_t sendData[NUM_BYTES + 1] = { 0x55, 0xaa, 0x00 };
-/*! \brief Buffer for test data reception. */
-uint8_t receivedData[NUM_BYTES + 1];
-/*! \brief Result of the example test. */
-bool success;
+///*** APP NOTE CODE ***/
+///*! \brief Number of test data bytes. */
+//#define NUM_BYTES   2
+///* Global variables. */
+///*! \brief SPI master on PORT C. */
+//SPI_Master_t spiMasterC;
+///*! \brief SPI slave on PORT D. */
+//SPI_Slave_t spiSlaveD = {NULL, NULL};
+///*! \brief Data packet. */
+//SPI_DataPacket_t dataPacket;
+///*! \brief Test data to send. */
+//const uint8_t sendData[NUM_BYTES + 1] = { 0x55, 0xaa, 0x00 };
+///*! \brief Buffer for test data reception. */
+//uint8_t receivedData[NUM_BYTES + 1];
+///*! \brief Result of the example test. */
+//bool success;
 /*** END APP NOTE CODE ***/
 
+/*
 typedef struct
 {
     uint8_t start;
@@ -91,7 +93,10 @@ typedef struct
     uint8_t * data;
     uint8_t * crc;
 } imu_rx_pkt;
+*/
 
+/*
 void mot_tx(mot_tx_pkt mot);
 void imu_tx(imu_tx_pkt imu);
 void imu_rx(imu_rx_pkt imu);
+*/
