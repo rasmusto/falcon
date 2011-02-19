e79d48399025de564432911c65528eb6c81442c1
Torben Rasmussen <rtorben@gmail.com>
2011-02-19 19:47:03 +0000

    removed files that donn't work

--- firmware/fcu/cli.h
+++
@@ -1,9 +0,0 @@
-void print_mot_pkts(volatile struct mot_tx_pkt_t * tx_pkt, volatile struct mot_rx_pkt_t * rx_pkt);
-void print_imu_pkts(volatile struct imu_tx_pkt_t * tx_pkt, volatile struct imu_rx_pkt_t * rx_pkt);
-
-void run_cmd(float val, char * (* function_ptr)(float));
-char * help(float val);
-char * do_nothing(float val);
-char * reboot(float val);
-char * green_led_on(float val);
-void process_rx_buf(volatile char * rx_buf);
