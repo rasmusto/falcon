5e6de8cd35b0e235d8d3e0e0364025eba112886b
Sarah Cooley <sarah2827@gmail.com>
2011-02-21 22:05:54 +0000

    rpy rather than xyz

--- software/kalman/matlab/demo.m
+++ software/kalman/matlab/demo.m
@@ -1,29 +1,30 @@
 % Pitch Tilt Yaw correction
 % State = (roll pitch yaw xacc yacc zacc). We only observe (xacc yacc zacc)
+% dirrectly
 
 % X(t+1) = F X(t) + noise(Q)
 % Y(t) = H X(t) + noise(R)
 
 ss = 6; % state size
 os = 3; % observation size
-F = [.9 0 .8 0 1 0;
-     0 1 0 1 0 1; 
-     0 0 1 0 1 0; 
-     0 0 0 1 0 1;
+F = [1 0 0 0 0 0;
+     0 1 0 0 0 0; 
+     0 0 1 0 0 0; 
+     0 0 0 1 0 0;
      0 0 0 0 1 0;
      0 0 0 0 0 1];
  
-H = [1 0 0 0 0 0;
-     0 1 0 0 0 0;
-     0 0 1 0 0 0];
+H = [0 0 0 1 0 0;
+     0 0 0 0 1 0;
+     0 0 0 0 0 1];
  
 Q = 0.01*eye(ss);
 R = 3*eye(os);
-initx = [10 2 5 0 0 0]';
+initx = [0 0 0 0 0 0];
 initV = 10*eye(ss);
 
-T = 40;
-[x,y] = sample_lds(F, H, Q, R, initx, T);
+T = size(data, 1);
+y = data;
 
 [xfilt, Vfilt, VVfilt, loglik] = kalman_filter(y, F, H, Q, R, initx, initV);
 [xsmooth, Vsmooth] = kalman_smoother(y, F, H, Q, R, initx, initV);
